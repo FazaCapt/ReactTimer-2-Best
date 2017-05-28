7357157a2b578ffefd984d08c215588c716af03d
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-28 16:05:53 +0700

    Testing Our Form With Spies (Add countdown form component and tests)

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -1,16 +1,12 @@
 var React = require('react');
 var Navigation = require('Navigation');
 
-
-
 var Main = (props) => {
     return(    
     <div>
         <Navigation />
         <div className="row">
             <div className="column small-centered medium-6 large-4">
-                
-                <p>Main.jsx rendered</p>
                 {props.children}
             </div>
         </div>
