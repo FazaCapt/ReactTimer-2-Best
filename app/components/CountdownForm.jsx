97506c4f17e0f5d2c7d8169fb16ad678a6457f42
faza Fahamsyah <fazafahamsyah@gmail.com>
2017-05-30 10:50:41 +0700

    Foundation Settings & Karma Issues (MASIH BELUM KELAR FOUNDATIONNYA BELUM BEKERJA)

--- app/components/CountdownForm.jsx
+++ app/components/CountdownForm.jsx
@@ -3,9 +3,10 @@ var React = require('react');
 var CountdownForm = React.createClass({
     onSubmit: function(e){
         e.preventDefault();
-
         var strSeconds = this.refs.seconds.value;
 
+        console.log('input count', $('input').length);
+        
         if(strSeconds.match(/^[0-9]*$/)){
             this.refs.seconds.value = '';
             this.props.onSetCountdown(parseInt(strSeconds, 10));
@@ -23,6 +24,6 @@ var CountdownForm = React.createClass({
             
         )
     }
-});
+}); 
 
 module.exports = CountdownForm;
